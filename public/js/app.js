e40d83367ea3384895ec988de43bbfe3ed430f97
Dreez <Hedrees05@gmail.com>
2022-08-01 17:45:15 -0700

    Init

--- public/js/app.js
+++ public/js/app.js
@@ -5,17 +5,17 @@ fetch('http://puzzle.mead.io/puzzle').then((response) => {
         console.log(data)
     })
 })
-fetch('http://localhost:3000/weather?address=!').then((response) => {
-    response.json().then((data) => {
-        if(data.error){
-            console.log(data.error)
-        }
-        else{
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
+// fetch('http://localhost:3000/weather?address=').then((response) => {
+//     response.json().then((data) => {
+//         if(data.error){
+//             console.log(data.error)
+//         }
+//         else{
+//             console.log(data.location)
+//             console.log(data.forecast)
+//         }
+//     })
+// })
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne=document.querySelector('#message-1')
